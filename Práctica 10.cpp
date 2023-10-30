2a7bf0b886de82c92f25c9a2602173e7632ebb8a
Cesar Adrian Zavala Perez <cesaradrizavperez@gmail.com>
2023-10-29 23:31:07 -0600

    Agregar archivos de proyecto.

---
+++ Práctica 10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int sumaIterativa(int n)
+{
+    int suma = 0;
+
+    while (n > 9)
+    {
+        suma += n % 10;
+        n /= 10;
+    }
+    return (suma + n);
+}
+
+int main()
+{
+    int numero = 12345;
+    int resultado = sumaIterativa(numero);
+    std::cout << "La suma de los digitos de " << numero << " es " << resultado << std::endl;
+
+    return 0;
+}
